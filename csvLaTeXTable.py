c6d9ca3f201298eb2f6c16a8379964c2354123e8
KOLANICH <kolan_n@mail.ru>
2018-05-15 13:40:44 +0300

    Added custom table tag support

--- csvLaTeXTable.py
+++ csvLaTeXTable.py
@@ -29,22 +29,29 @@ class Parser(object):
     
     def print_table_heading(self):
         '''Prints the LaTeX heading for the table'''
-        self.write("\\begin{table}")
+        if self.options.custom:
+            self.write("\\begin{"+self.options.custom+"}")
+        else:
+            self.write("\\begin{table}")
         if self.options.position != "":
             if self.options.position[0] == "[" and self.options.position[-1] == "]":
                 self.write(self.options.position)
             else:
                 self.write("[" + self.options.position + "]")
-        self.writeln("")
-        if self.options.centering:
-            self.writeln("\\centering",1)
-        self.write("\\begin{" + self.options.environment + "}",1)
+        if not self.options.custom:
+            self.writeln("")
+            if self.options.centering:
+                self.writeln("\\centering",1)
+            self.write("\\begin{" + self.options.environment + "}",1)
     
     def print_table_ending(self):
         '''Prints the LaTeX ending for the table'''
         self.writeln("\\hline",2)
-        self.writeln("\\end{" + self.options.environment + "}",1)
-        self.writeln("\\end{table}")
+        if self.options.custom:
+            self.writeln("\\end{"+self.options.custom+"}")
+        else:
+            self.writeln("\\end{" + self.options.environment + "}",1)
+            self.writeln("\\end{table}")
 
     def print_table_format(self,line):
         '''Prints the format of the table, based on splitting the first line by the delimiter'''
@@ -115,11 +122,12 @@ def main():
     optionparser.add_option("-f", "--file",dest="filename",default="",help="Specify the file to convert.")
     optionparser.add_option("-n", "--no-format",dest="no_format",default=False,action="store_true",help="Do not format the source written out.")
     optionparser.add_option("-T", "--table-spec",dest="table_spec",default="",help="Specify the table spec to use (such as l for left justified, c for centered, etc.). Any strings with a pipe will be treated as the entire spec. Defaults to 'l'.")
+    optionparser.add_option("--environment",dest="environment",type="string", default=None, help="Specify the table environment.")
     optionparser.add_option("-x", "--tabularx",dest="tabularx",default=False,action="store_true",help="Use the tabularx environment instead of the tabular environment.")
     optionparser.add_option("-m", "--multiline",dest="multiline",default=False,action="store_true",help="Use multiline headers with dynamic expanding. The table spec defaults to X when using this option.")
     optionparser.add_option("-c", "--centering",dest="centering",default=False,action="store_true",help="Center the table.")
     optionparser.add_option("-p", "--position",dest="position",default="",help="Set the float position of the table (h,H,H!)")
-
+    optionparser.add_option("--custom", dest="custom", type="string", default="longtable", help="Don't use table and tabular, use a custom tag with longtable-compatible interface instead")
 
 
     (options,args) = optionparser.parse_args()
@@ -127,12 +135,13 @@ def main():
     #If the tab flag is set, change the delimiter
     if options.use_tab:
         options.delimiter = "\t"
-        
-    #If the tabularx flag is set, use the tabularx environment
-    if options.tabularx:
-        options.environment = "tabularx"
-    else:
-        options.environment = "tabular"
+    
+    if options.environment is None:
+        #If the tabularx flag is set, use the tabularx environment
+        if options.tabularx:
+            options.environment = "tabularx"
+        else:
+            options.environment = "tabular"
 
     #If the multiline options is set make sure the table_spec defaults to X
     if options.multiline:
